ba465eca415ed5d2f8083ba14204a4583ae18950
Gabriel Batalha <gabrielbatalha0902@gmail.com>
2023-02-01 22:14:05 -0300

    added images

--- web/src/assets/index.ts
+++ web/src/assets/index.ts
@@ -1,4 +1,5 @@
 import Logo from "./LogoSaveAccount.png";
 import WorkAbroad from "./WorkAbroad.png";
+import NotFound from "./NotFound.svg";
 
-export { Logo, WorkAbroad };
+export { Logo, WorkAbroad, NotFound };
