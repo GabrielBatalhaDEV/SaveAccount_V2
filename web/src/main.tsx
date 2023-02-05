5e7a89cf5bb78b4bb4e6869cc55f0a59e2d10e28
Gabriel Batalha <gabrielbatalha0902@gmail.com>
2023-02-05 20:50:06 -0300

    Added react query provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./lib/reactQuery";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
